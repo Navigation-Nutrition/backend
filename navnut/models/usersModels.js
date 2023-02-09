300af1ec3867352ac226de7240cb9cb84e7e2e83
Celybot <youancece@gmail.com>
2023-02-09 14:48:16 -0500

    gets friends list by user is

--- navnut/models/usersModels.js
+++ navnut/models/usersModels.js
@@ -8,10 +8,14 @@ class usersModel {
   }
   static async getUserIdFromDB(userID){
     let query = await pool.query("SELECT * FROM users WHERE user_id = $1", [userID])
-    //console.log(query, "USER ID DB")
+    console.log(query, "USER ID DB")
     return query.rows[0]
   }
-
+  static async getUsersFriendsFromDB(userID){
+    const query = await pool.query("SELECT * FROM users JOIN friends ON friends.friends_id = users.user_id WHERE friends.user_id = $1", [userID])
+    console.log(query)
+    return query.rows[0]
+  }
 }
 
 module.exports = usersModel
--- navnut/routes/usersRoutes.js
+++ navnut/routes/usersRoutes.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { getAllUsers } = require("../controllers/usersController")
 const { getUsersByID } = require("../controllers/usersController")
+const { getUsersFriendsByID } = require("../controllers/usersController")
+
 
 router.get("/", getAllUsers)
 router.get("/:userID", getUsersByID)
+router.get("/:userID/friends", getUsersFriendsByID)
 
 module.exports = router
